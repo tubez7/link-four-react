83ea3d3f4face69b158390d3c3d3005c6f3d0b54
tubez7 <thescopeboys@hotmail.com>
2022-12-06 20:57:25 +0000

    add about page and nav links

---
+++ src/components/About.jsx
@@ -0,0 +1,4 @@
+export default function About(props) {
+    console.log("props", props);
+    return <div><p>Here is some placeholder txt. Blah blah blah....</p></div>
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,42 @@
-export default function Nav({ visible, setVisible }) {
+import { useNavigate } from "react-router-dom";
+
+export default function Nav({
+  visible,
+  setVisible,
+  aboutClicked,
+  setAboutClicked,
+}) {
   const newGameForm = () => {
     setVisible(false);
   };
 
+  const navigate = useNavigate();
+
+  const handleAboutClicked = (e) => {
+    e.preventDefault();
+    navigate("/about");
+    setAboutClicked(true);
+  };
+
+  const handleBackClicked = (e) => {
+    e.preventDefault();
+    navigate(-1);
+    setAboutClicked(false);
+  };
+
   return (
     <nav className="nav-bar">
-      <button onClick={newGameForm} disabled={!visible}>New Game</button>
-      <button>About/Info</button>
+      {!aboutClicked && (
+        <>
+          <button onClick={newGameForm} disabled={!visible}>
+            New Game
+          </button>
+          <button onClick={handleAboutClicked}>About/Info</button>
+        </>
+      )}
+      {aboutClicked && (
+        <button onClick={handleBackClicked}>Back to game</button>
+      )}
     </nav>
   );
 }
