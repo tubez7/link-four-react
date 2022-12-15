67ac7e2fdae84287160dbd4bc69531f0e12d120a
tubez7 <thescopeboys@hotmail.com>
2022-12-15 19:15:07 +0000

    update info-list ui/ux

--- src/components/InfoList.jsx
+++ src/components/InfoList.jsx
@@ -2,6 +2,9 @@ import { useContext } from "react";
 import { CurrentPlayerContext } from "../contexts/CurrentPlayer";
 import { PlayerContext } from "../contexts/Player";
 
+import p1Icon from "../images/28x28cyan.png";
+import p2Icon from "../images/28x28yellow.png";
+
 export default function InfoList({
   player1Score,
   player2Score,
@@ -21,14 +24,16 @@ export default function InfoList({
     <div className="info-list">
       <ul className="list-items">
         <li>
-          <h4>Players</h4>
-          <p className="p1-info">P1: {player1} (x)</p>
-          <p className="p2-info">P2: {player2} (o)</p>
+          <h4>Players<br className="line-break"/><br className="turn-break"/></h4>
+          <p className="p1-info">{player1}</p>
+          <img alt="p1-icon" src={p1Icon} />
+          <p className="p2-info">{player2}</p>
+          <img alt="p2-icon" src={p2Icon} />
         </li>
         <li className="score-info">
           <h4>Current Score</h4>
-          <p className="p1-score">{`${player1Score}`}</p>
-          <p className="p2-score">{`${player2Score}`}</p>
+          <span className="p1-score">{`${player1Score} `}</span>v
+          <span className="p2-score">{` ${player2Score}`}</span>
         </li>
         <li className="game-count">
           <h4>Game No.</h4>
@@ -39,7 +44,10 @@ export default function InfoList({
           <p>{turnCount}</p>
         </li>
         <li>
-          <h4>Current Turn</h4>
+          <h4>
+            Current
+            <br className="line-break" /> Turn
+          </h4>
           <p className={`p${currentPlayer}-turn`}>
             {currentPlayer === 1 ? player1 : player2}
           </p>
